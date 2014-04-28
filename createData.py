cc18b557a197692fcb6e722d14b4b01a1f2d32a3
connor <connor@cs.brown.edu>
2014-04-28 17:07:50 -0400

    Improve rendering

--- createData.py
+++ createData.py
@@ -7,6 +7,28 @@ import sys
 
 from PIL import Image
 
+# returns RGB value given a [0,1] value
+def getRGBValue(c):
+  def interpolate(c1, c2, s):
+    def d(i):
+      return c2[i]-int((c2[i]-c1[i])*s)
+
+    final = (d(0), d(1), d(2))
+    return final
+
+  G = (56, 180, 73)
+  B = (27, 117, 187)
+  Y = (255, 241, 0)
+
+  #return interpolate(B,Y,c)
+
+  if c < .5:
+    return interpolate(B,G,c*2)
+  else:
+    return interpolate(Y,B,(c-.5)*2)
+
+
+
 # from http://stackoverflow.com/questions/16834861/create-own-colormap-using-matplotlib-and-plot-color-scale
 def make_colormap(seq):
     """Return a LinearSegmentedColormap
@@ -40,6 +62,29 @@ def makeCluster(xMean, yMean, xCovar, yCovar, numPts):
   makeInt = np.vectorize(lambda x: float(int(x)))
   return makeInt(x),makeInt(y)
 
+def makeIrregularCluster(xMean, yMean,xCovar, yCovar, numPts, numCentroids):
+  if numCentroids < 1:
+    return
+  xs,ys = makeCluster(xMean, yMean,xCovar, yCovar, numPts)
+
+  if numCentroids < 2:
+    return
+  for c in range(numCentroids-1):
+    xSign = -1 if np.random.random() < .5 else 1
+    ySign = -1 if np.random.random() < .5 else 1
+    # xSign = ySign = 1
+
+    xOffset = np.random.random_integers(min(xMean/10, xCovar/10), xCovar/4)
+    yOffset = np.random.random_integers(min(yMean/10, yCovar/10), yCovar/4)
+    tmpXMean = xMean + (xSign * xOffset)
+    tmpYMean = yMean + (ySign * yOffset)
+
+    tmpXs, tmpYs = makeCluster(tmpXMean, tmpYMean, xCovar, yCovar, numPts)
+    xs = np.concatenate((xs, tmpXs))
+    ys = np.concatenate((ys, tmpYs))
+  makeInt = np.vectorize(lambda x: float(int(x)))
+  return makeInt(xs), makeInt(ys)
+
 def applyNoise(ptFrequency):
   # Add uniform noise
   # numNoisePts = 1000000.
@@ -49,7 +94,7 @@ def applyNoise(ptFrequency):
   # for x,y in zip(noise_x, noise_y):
   #   ptFrequency[x][y] = ptFrequency[x][y] + 1
 
-  noise = makeNormalNoise(40, 5, setSize)
+  noise = makeNormalNoise(40, 3, setSize)
   for x,freqX in zip(ptFrequency.keys(), noise):
     for y, freqVal in zip(ptFrequency[x], freqX):
       if ptFrequency[x][y] == baseCount:
@@ -69,18 +114,35 @@ for x in ptFrequency.keys():
   for y in range(0, setSize):
     ptFrequency[x][y] = baseCount
 
+################################################################################
+# ADDITIVE CLUSTERS
 # make and add the first cluster
-clusterPts = 100000
-xs, ys = makeCluster(160, 160, 200, 200, clusterPts)
+clusterPts = 25000
+# xs, ys = makeCluster(160, 160, 200, 200, clusterPts)
+# for x,y in zip(xs,ys):
+#   ptFrequency[x][y] = ptFrequency[x][y] + 1
+
+xs, ys = makeIrregularCluster(200, 160, 200, 200, clusterPts, 6)
 for x,y in zip(xs,ys):
+  if x >= setSize or y >= setSize:
+    continue
   ptFrequency[x][y] = ptFrequency[x][y] + 1
 
+################################################################################
+# SUBTRACTIVE CLUSTERS
 # make and subtract the second cluster
-xs, ys = makeCluster(400, 400, 200, 200, clusterPts)
+# xs, ys = makeCluster(400, 400, 200, 200, clusterPts)
+# for x,y in zip(xs,ys):
+#   ptFrequency[x][y] = ptFrequency[x][y] - 1
+
+xs, ys = makeIrregularCluster(380, 380, 200, 200, clusterPts, 2)
 for x,y in zip(xs,ys):
+  if x >= setSize or y >= setSize:
+    continue
   ptFrequency[x][y] = ptFrequency[x][y] - 1
 
-# create and add noise
+################################################################################
+# NOISE
 ptFrequency = applyNoise(ptFrequency)
 
 # Create np.array for x and y; this can be improved speed-wise
@@ -95,9 +157,8 @@ pixels = img.load()
 
 for i in range(img.size[0]):    # for every pixel:
   for j in range(img.size[1]):
-    #rgb = (i, j, 100)
-    normVal = int((ptFrequency[i][j] / float(maxFreq)) * 255)
-    rgb = (normVal,normVal,normVal)
+    normVal = (((ptFrequency[i][j] - baseCount)/baseCount)+1)/2
+    rgb = getRGBValue(normVal)
     pixels[i,j] = rgb # set the colour accordingly
 
 
